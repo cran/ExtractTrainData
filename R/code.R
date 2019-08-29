0050eac7a0074ea8dfd39c1b06591f4a365a9a3b
Subhadip Datta <subhadipdatta007@gmail.com>
2019-08-29 10:00:02 +0000

    version 5.0.1

---
+++ R/code.R
@@ -0,0 +1,36 @@
+#' At the time of use this you have to add a Polygon shapefile.
+#' @author  Subhadip Datta
+#' @param ras_img Raster image
+#' @param poly_shp Polygon shapefile with class info
+#' @param res_Col_name Name of the colomn contain class id's
+#' @import raster
+#' @import rgeos
+#' @import rgdal
+#' @examples
+#' library(raster)
+#' library(ExtractTrainData)
+#' ras_img<-brick(system.file("extdata","ras.tif",package = "ExtractTrainData"))
+#' poly_shp<-shapefile(system.file("extdata","poly_shp.shp",package = "ExtractTrainData"))
+#' res_Col_name<-"Id"
+#' ExValue_from_Polyshp(ras_img,poly_shp,res_Col_name)
+#' @export
+ExValue_from_Polyshp<-function(ras_img,poly_shp,res_Col_name){
+  message("No. of bands - ",length(names(ras_img)))
+  aLl<-data.frame()
+  poly_shp[[res_Col_name]]<-as.integer(poly_shp[[res_Col_name]])
+  val<-unique(poly_shp[[res_Col_name]])
+  message("No. of classes - ",length(val))
+  for(i in 1:length(val)){
+    message(".", appendLF=FALSE)
+    ext_val<-extract(ras_img,poly_shp[poly_shp[[res_Col_name]]==val[i],],df=T)[,-1]
+    message(".", appendLF=FALSE)
+    ext_val$class_id<-val[i]
+    message(".", appendLF=FALSE)
+    aLl<-rbind(aLl,ext_val)
+    message(".", appendLF=FALSE)
+    remove(ext_val)
+    message(".",round((i/length(val))*100),"% ", appendLF=FALSE)
+  }
+  message("\nExtraction Completed")
+  aLl
+}
---
+++ R/code2.R
@@ -0,0 +1,36 @@
+#' At the time of use this you have to add a Point shapefile.
+#' @author  Subhadip Datta
+#' @param ras_img Raster image
+#' @param point_shp Point shapefile with class info
+#' @param res_Col_name Name of the colomn contain class id's
+#' @import raster
+#' @import rgeos
+#' @import rgdal
+#' @examples
+#' library(raster)
+#' library(ExtractTrainData)
+#' ras_img<-brick(system.file("extdata","ras.tif",package = "ExtractTrainData"))
+#' point_shp<-shapefile(system.file("extdata","poin.shp",package = "ExtractTrainData"))
+#' res_Col_name<-"Id"
+#' ExValue_from_Pointshp(ras_img,point_shp,res_Col_name)
+#' @export
+ExValue_from_Pointshp<-function(ras_img,point_shp,res_Col_name){
+  message("No. of bands - ",length(names(ras_img)))
+  aLl<-data.frame()
+  point_shp[[res_Col_name]]<-as.integer(point_shp[[res_Col_name]])
+  val<-unique(point_shp[[res_Col_name]])
+  message("No. of classes - ",length(val))
+  for(i in 1:length(val)){
+    message(".", appendLF=FALSE)
+    trp<-extract(ras_img,point_shp[point_shp[[res_Col_name]]==val[i],],df=T)[,-1]
+    message(".", appendLF=FALSE)
+    trp$class_id<-val[i]
+    message(".", appendLF=FALSE)
+    aLl<-rbind(aLl,trp)
+    message(".", appendLF=FALSE)
+    remove(trp)
+    message(".",round(i/length(val)*100),"% ", appendLF=FALSE)
+  }
+  message("\nExtraction Completed")
+  aLl
+}
